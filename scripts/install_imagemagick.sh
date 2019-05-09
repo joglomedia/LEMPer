2aa0b249c3b6e408341387351d7bc852b5e6eb10
Eddie Septr <eslabs.id@gmail.com>
2019-05-09 23:33:47 +0700

    Add ImageMagick installer

---
+++ scripts/install_imagemagick.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Include decorator
+if [ "$(type -t run)" != "function" ]; then
+    BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+    . ${BASEDIR}/decorator.sh
+fi
+
+# Make sure only root can run this installer script
+if [ $(id -u) -ne 0 ]; then
+    error "This script must be run as root..."
+    exit 1
+fi
+
+echo "Installing ImageMagick from source..."
+
+run wget https://www.imagemagick.org/download/ImageMagick.tar.gz
+run tar xf ImageMagick.tar.gz
+run cd ImageMagick-7*
+./configure
+run make
+run make install
+run ldconfig /usr/local/lib
+
+run rm -fr ImageMagick-7*
+
+if [ -n "$(magick -version |grep -o 'Version: ImageMagic *')" ]; then
+    status "ImageMagic version $(magick -version |grep ^Version | cut -d' ' -f3) has been installed"
+elif [ -n "$(identify -version |grep -o 'Version: ImageMagic *')" ]; then
+    status "ImageMagic version $(identify -version |grep ^Version | cut -d' ' -f3) has been installed"
+fi
