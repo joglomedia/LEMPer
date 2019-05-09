55e6419aa8491d1aa456b21aeb400e5544764a7d
Eddie Septr <eslabs.id@gmail.com>
2019-05-09 23:26:45 +0700

    Update Admin tools

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,43 +1,56 @@
 #!/usr/bin/env bash
 
-echo "Installing addon tools..."
+# Include decorator
+if [ "$(type -t run)" != "function" ]; then
+    BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+    . ${BASEDIR}/decorator.sh
+fi
 
-# Install Nginx Vhost Creator
-cp -f scripts/ngxvhost.sh /usr/local/bin/ngxvhost
-cp -f scripts/ngxvhost.sh /usr/local/bin/ngxrmvhost
-chmod ugo+x /usr/local/bin/ngxvhost
-chmod ugo+x /usr/local/bin/ngxrmvhost
+# Make sure only root can run this installer script
+if [ $(id -u) -ne 0 ]; then
+    error "This script must be run as root..."
+    exit 1
+fi
+
+echo "Installing Administration tools..."
+
+# Install Nginx vHost Creator
+run cp -f scripts/ngxvhost.sh /usr/local/bin/ngxvhost
+run cp -f scripts/ngxtool.sh /usr/local/bin/ngxtool
+run chmod ugo+x /usr/local/bin/ngxvhost
+run chmod ugo+x /usr/local/bin/ngxtool
 
 # Install Web-viewer Tools
-mkdir /usr/share/nginx/html/tools/
+run mkdir /usr/share/nginx/html/tools/
+
+# Install PHP Info
+#cat > /usr/share/nginx/html/tools/phpinfo.php <<EOL
+#<?php phpinfo(); ?>
+#EOL
+run bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php'
+run bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php70'
+run bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php71'
+run bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php72'
+run bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php73'
 
 # Install Zend OpCache Web Viewer
-wget --no-check-certificate https://raw.github.com/rlerdorf/opcache-status/master/opcache.php -O /usr/share/nginx/html/tools/opcache.php
+run wget --no-check-certificate https://raw.github.com/rlerdorf/opcache-status/master/opcache.php -O /usr/share/nginx/html/tools/opcache.php
 
 # Install Memcache Web-based stats
 #http://blog.elijaa.org/index.php?pages/phpMemcachedAdmin-Installation-Guide
-git clone https://github.com/elijaa/phpmemcachedadmin.git /usr/share/nginx/html/tools/phpMemcachedAdmin/
+run git clone https://github.com/elijaa/phpmemcachedadmin.git /usr/share/nginx/html/tools/phpMemcachedAdmin/
 
 # Install Adminer for Web-based MySQL Administration Tool
-mkdir /usr/share/nginx/html/tools/adminer/
-wget --no-check-certificate https://github.com/vrana/adminer/releases/download/v4.7.1/adminer-4.7.1.php -O /usr/share/nginx/html/tools/adminer/index.php
-wget --no-check-certificate https://github.com/vrana/adminer/releases/download/v4.7.1/editor-4.7.1.php -O /usr/share/nginx/html/tools/adminer/editor.php
+run mkdir /usr/share/nginx/html/tools/adminer/
+run wget --no-check-certificate https://github.com/vrana/adminer/releases/download/v4.7.1/adminer-4.7.1.php -O /usr/share/nginx/html/tools/adminer/index.php
+run wget --no-check-certificate https://github.com/vrana/adminer/releases/download/v4.7.1/editor-4.7.1.php -O /usr/share/nginx/html/tools/adminer/editor.php
 
-# Install PHP Info
-cat > /usr/share/nginx/html/tools/phpinfo.php <<EOL
-<?php phpinfo(); ?>
-EOL
+# Install FileRun File Manager
+run mkdir /usr/share/nginx/html/tools/filerun/
+run wget -O FileRun.zip http://www.filerun.com/download-latest
+run unzip FileRun.zip -d /usr/share/nginx/html/tools/filerun/
+run rm -f FileRun.zip
 
-cat > /usr/share/nginx/html/tools/phpinfo.php70 <<EOL
-<?php phpinfo(); ?>
-EOL
-
-cat > /usr/share/nginx/html/tools/phpinfo.php71 <<EOL
-<?php phpinfo(); ?>
-EOL
-
-sudo bash -c 'echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/tools/phpinfo.php72'
-
-cat > /usr/share/nginx/html/tools/phpinfo.php73 <<EOL
-<?php phpinfo(); ?>
-EOL
+if [[ -x /usr/local/bin/ngxvhost && -x /usr/local/bin/ngxtool && -d /usr/share/nginx/html/tools ]]; then
+    status "Web administration tools successfully installed."
+fi
