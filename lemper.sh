2d18f4100f46a592375535e23ab6369142a02ebf
Eddie Septr <eslabs.id@gmail.com>
2019-07-02 01:20:03 +0700

    Updates PHP uninstalller

--- lemper.sh
+++ lemper.sh
@@ -373,7 +373,7 @@ case $1 in
         fi
 
         # Remove default user
-        echo -e "\nRemove default LEMPer account? [Y/n]: "
+        echo -e "Remove default LEMPer account? [Y/n]: "
         read rmdefaultuser
         if [[ "${rmdefaultuser}" == Y* || "${rmdefaultuser}" == y* ]]; then
             if [[ -z $(getent passwd "${username}") ]]; then
