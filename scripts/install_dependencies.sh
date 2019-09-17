34c9d5a313dc287f6704bbdbce3360be8c801b7e
joglomedia <eslabs.id@gmail.com>
2019-09-17 16:48:32 +0700

    update dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -26,7 +26,7 @@ if hash apt-get 2>/dev/null; then
     # Install dependencies.
     run apt-get -qq install -y apache2-utils apt-transport-https bash build-essential ca-certificates cron curl \
         git gnupg2 iptables libgd-dev libgeoip-dev libssl-dev libxml2-dev libpcre3-dev libxslt1-dev lsb-release \
-        openssh-server openssl rsync software-properties-common snmp sudo sysstat tar unzip wget whois
+        openssh-server openssl rsync software-properties-common sasl2-bin snmp sudo sysstat tar unzip wget whois
 elif hash yum 2>/dev/null; then
     fail "Unable to install LEMPer: yum distribution is not supported yet."
 
