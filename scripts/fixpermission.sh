69d62fc2408f36ebd996a4b70428b798f5e9646c
joglomedia <eslabs.id@gmail.com>
2019-11-22 11:24:42 +0700

    helper fix file permission

--- scripts/fixpermission.sh
+++ scripts/fixpermission.sh
@@ -2,7 +2,7 @@
 # usage:
 #####################################################
 # directory
-[ "$1" = "" ]  && return 0
+[ "$1" = "" ] && return 0
 
-find $1 -type d -print0 | xargs -0 chmod 755
-find $1 -type f -print0 | xargs -0 chmod 644
+find "$1" -type d -print0 | xargs -0 chmod 755
+find "$1" -type f -print0 | xargs -0 chmod 644
