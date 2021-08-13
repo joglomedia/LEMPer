e89fd7e263dd94b9614f7399c6daf11edf5db1f7
Edi Septriyanto <me@masedi.net>
2021-08-13 09:25:49 +0700

    Fix file sourcing

--- remove.sh
+++ remove.sh
@@ -32,7 +32,7 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 
 # Include helper functions.
 if [ "$(type -t run)" != "function" ]; then
-    . scripts/helper.sh
+    . ./scripts/helper.sh
 fi
 
 # Make sure only root can run this installer script.
