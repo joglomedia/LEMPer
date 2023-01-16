da44071583595d9460e65453dd2692dac376d69b
Edi Septriyanto <me@masedi.net>
2023-01-17 05:18:06 +0700

    Add PHP 8.2 support

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -999,7 +999,7 @@ function init_php_install() {
                 ;;
                 10 | "all")
                     # Select all PHP versions (except EOL & Beta).
-                    SELECTED_PHP_VERSIONS=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1", "8.2")
+                    SELECTED_PHP_VERSIONS=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2")
                 ;;
                 *)
                     error "Your selected PHP version ${SELECTED_PHP} is not supported yet."
