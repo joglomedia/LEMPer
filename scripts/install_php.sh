2a9774aa5a118bcc761bc2b0e63bb7611054086e
Edi Septriyanto <me@masedi.net>
2024-07-07 22:42:32 +0700

    Update: list of supported  PHP versions

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -1050,7 +1050,7 @@ function init_php_install() {
                 ;;
                 9 | "all")
                     # Select all PHP versions (except EOL & Beta).
-                    SELECTED_PHP_VERSIONS=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2")
+                    SELECTED_PHP_VERSIONS=("7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2" "8.3")
                 ;;
                 *)
                     error "Your selected PHP version ${SELECTED_PHP} is not supported yet."
