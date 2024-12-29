40f6339cad0d571144455479310bcf5f9ca330d7
Edi Septriyanto <me@masedi.net>
2024-12-29 23:55:32 +0700

    Add Certbot logrotate

--- scripts/install_certbotle.sh
+++ scripts/install_certbotle.sh
@@ -67,50 +67,20 @@ function init_certbotle_install() {
             ;;
         esac
 
-        # Add Certbot auto renew command to cronjob.
-        if [[ "${DRYRUN}" != true ]]; then
-            export EDITOR=nano
-            CRONCMD='0 */6 * * * /usr/bin/certbot renew --quiet --renew-hook "/usr/sbin/service nginx reload -s"'
-            touch lemper.cron
-            crontab -u root lemper.cron
-            crontab -l > lemper.cron
-
-            if ! grep -qwE "/usr/bin/certbot\ renew" lemper.cron; then
-                cat >> lemper.cron <<EOL
-# LEMPer Cronjob
-# Certbot Auto-renew Let's Encrypt certificates.
-SHELL=/bin/bash
-PATH=PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=root
-
-${CRONCMD}
-EOL
-
-                crontab lemper.cron
-                rm -f lemper.cron
-                #service cron reload
-            else
-                info "Certbot auto-renew command added to cronjob in dry run mode."
-            fi
-
-            # Register a new account.
-            local LE_EMAIL=${LEMPER_ADMIN_EMAIL:-"cert@lemper.cloud"}
-
-            if [[ -d /etc/letsencrypt/accounts/acme-v02.api.letsencrypt.org/directory ]]; then
-                run certbot update_account --email "${LE_EMAIL}" --no-eff-email --agree-tos
-            else
-                run certbot register --email "${LE_EMAIL}" --no-eff-email --agree-tos
-            fi
-        fi
-
-        # Self-signed OpenSSL cert config.
-        run mkdir -p "/etc/lemper/ssl/${HOSTNAME}" && \
-        run cp -f etc/openssl/ca.conf /etc/lemper/ssl/ca.conf && \
-        run cp -f etc/openssl/csr.conf /etc/lemper/ssl/csr.conf && \
-        run cp -f etc/openssl/cert.conf /etc/lemper/ssl/cert.conf
-
         if [[ "${DRYRUN}" != true ]]; then
             if certbot --version | grep -q "certbot"; then
+                # Register a new Letsencrypt's account.
+                local LE_EMAIL=${LEMPER_ADMIN_EMAIL:-"cert@lemper.cloud"}
+
+                if [[ -d /etc/letsencrypt/accounts/acme-v02.api.letsencrypt.org/directory ]]; then
+                    run certbot update_account --email "${LE_EMAIL}" --no-eff-email --agree-tos
+                else
+                    run certbot register --email "${LE_EMAIL}" --no-eff-email --agree-tos
+                fi
+
+                add_certbot_cron
+                add_certbot_logrotate
+
                 success "Certbot successfully installed."
             else
                 info "Something went wrong with Certbot installation."
@@ -129,6 +99,57 @@ function install_certbot_pip() {
     run /opt/certbot/bin/pip install --upgrade pip setuptools cffi && \
     run /opt/certbot/bin/pip install --upgrade certbot certbot-nginx && \
     run ln -sf /opt/certbot/bin/certbot /usr/bin/certbot
+}
+
+##
+# Add Certbot auto renew command to cronjob.
+##
+function add_certbot_cron() {
+    export EDITOR=nano
+    CRONCMD='0 */6 * * * /usr/bin/certbot renew --quiet --renew-hook "/usr/sbin/service nginx reload -s"'
+    touch lemper.cron
+    crontab -u root lemper.cron
+    crontab -l > lemper.cron
+
+    if ! grep -qwE "/usr/bin/certbot\ renew" lemper.cron; then
+        cat >> lemper.cron <<EOL
+# LEMPer Cronjob
+# Certbot Auto-renew Let's Encrypt certificates.
+SHELL=/bin/bash
+PATH=PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+
+${CRONCMD}
+EOL
+
+        crontab lemper.cron
+        rm -f lemper.cron
+    else
+        info "Certbot auto-renew command added to cronjob in dry run mode."
+    fi
+}
+
+##
+# Add logrotate for Certbot.
+##
+function add_certbot_logrotate() {
+    if [[ -f /etc/logrotate.d/certbot ]]; then
+        run rm -f /etc/logrotate.d/certbot
+    fi
+
+    run touch "/etc/logrotate.d/certbot"
+    cat >> "/etc/logrotate.d/certbot" <<EOL
+/var/log/letsencrypt/*.log {
+    weekly
+    rotate 12
+    compress
+    delaycompress
+    missingok
+    notifempty
+}
+EOL
+
+    run chmod 0644 "/etc/logrotate.d/certbot"
 }
 
 echo "[Certbot Let's Encrypt Installation]"
