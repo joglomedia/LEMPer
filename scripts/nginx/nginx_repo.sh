19a7067cb08d18c0f1d07c1de360af139a37cc99
Edi Septriyanto <me@masedi.net>
2026-02-01 00:02:45 +0700

    Refactor Nginx installer

--- scripts/nginx/nginx_repo.sh
+++ scripts/nginx/nginx_repo.sh
@@ -87,82 +87,84 @@ function add_nginx_repo_myguard() {
 }
 
 ##
-# Install extra module packages from repository
+# Build extra module packages list from repository
 # Populates EXTRA_MODULE_PKGS array based on enabled modules
+# Note: All debug messages go to stderr to avoid polluting package list
 ##
 function get_repo_extra_module_packages() {
     local SELECTED_REPO="${1:-ondrej}"
     EXTRA_MODULE_PKGS=()
 
-    echo "Determining extra module packages..."
+    echo "Determining extra module packages..." >&2
 
     # Auth PAM
     if "${NGX_HTTP_AUTH_PAM:-false}"; then
-        echo "Adding ngx-http-auth-pam module..."
+        echo "  Adding: libnginx-mod-http-auth-pam" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-auth-pam")
     fi
 
     # Brotli compression
     if "${NGX_HTTP_BROTLI:-false}"; then
-        echo "Adding ngx-http-brotli module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-brotli" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-brotli")
         else
+            echo "  Adding: libnginx-mod-brotli" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-brotli")
         fi
     fi
 
     # Cache Purge
     if "${NGX_HTTP_CACHE_PURGE:-false}"; then
-        echo "Adding ngx-http-cache-purge module..."
+        echo "  Adding: libnginx-mod-http-cache-purge" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-cache-purge")
     fi
 
     # DAV Ext
     if "${NGX_HTTP_DAV_EXT:-false}"; then
-        echo "Adding ngx-http-dav-ext module..."
+        echo "  Adding: libnginx-mod-http-dav-ext" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-dav-ext")
     fi
 
     # Echo
     if "${NGX_HTTP_ECHO:-false}"; then
-        echo "Adding ngx-http-echo module..."
+        echo "  Adding: libnginx-mod-http-echo" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-echo")
     fi
 
     # Fancy indexes
     if "${NGX_HTTP_FANCYINDEX:-false}"; then
-        echo "Adding ngx-http-fancyindex module..."
+        echo "  Adding: libnginx-mod-http-fancyindex" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-fancyindex")
     fi
 
     # GeoIP
     if "${NGX_HTTP_GEOIP:-false}"; then
-        echo "Adding ngx-http-geoip module..."
+        echo "  Adding: libnginx-mod-http-geoip" >&2
         EXTRA_MODULE_PKGS+=("libmaxminddb0" "libmaxminddb-dev" "libnginx-mod-http-geoip" "libnginx-mod-stream-geoip")
     fi
 
     # GeoIP2
     if "${NGX_HTTP_GEOIP2:-false}"; then
-        echo "Adding ngx-http-geoip2 module..."
+        echo "  Adding: libnginx-mod-http-geoip2" >&2
         EXTRA_MODULE_PKGS+=("libmaxminddb0" "libmaxminddb-dev" "libnginx-mod-http-geoip2" "libnginx-mod-stream-geoip2")
     fi
 
     # Headers more
     if "${NGX_HTTP_HEADERS_MORE:-false}"; then
-        echo "Adding ngx-http-headers-more-filter module..."
+        echo "  Adding: libnginx-mod-http-headers-more-filter" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-headers-more-filter")
     fi
 
     # Image filter
     if "${NGX_HTTP_IMAGE_FILTER:-false}"; then
-        echo "Adding ngx-http-image-filter module..."
+        echo "  Adding: libnginx-mod-http-image-filter" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-image-filter")
     fi
 
     # Lua
     if "${NGX_HTTP_LUA:-false}"; then
-        echo "Adding ngx-http-lua module..."
+        echo "  Adding: libnginx-mod-http-lua" >&2
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
             EXTRA_MODULE_PKGS+=("luarocks" "lua-cjson" "lua-resty" "lua-resty-core" "lua-resty-lrucache" "libnginx-mod-http-lua")
         else
@@ -177,22 +179,22 @@ function get_repo_extra_module_packages() {
 
     # NAXSI
     if "${NGX_HTTP_NAXSI:-false}"; then
-        echo "Adding ngx-http-naxsi module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-naxsi" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-naxsi")
         fi
     fi
 
     # NDK
     if "${NGX_HTTP_NDK:-false}"; then
-        echo "Adding ngx-http-ndk module..."
+        echo "  Adding: libnginx-mod-http-ndk" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-ndk")
     fi
 
     # NJS
     if "${NGX_HTTP_NJS:-false}"; then
-        echo "Adding ngx-http-njs module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-njs" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-njs")
         else
             error "${SELECTED_REPO} doesn't have libnginx-mod-http-njs module. Skipped..."
@@ -201,9 +203,8 @@ function get_repo_extra_module_packages() {
 
     # Passenger
     if "${NGX_HTTP_PASSENGER:-false}"; then
-        echo "Adding ngx-http-passenger module..."
         if [[ -n $(command -v passenger-config) ]]; then
-            echo "Passenger found..."
+            echo "  Passenger found..." >&2
         else
             error "Passenger not found. Skipped..."
         fi
@@ -211,8 +212,8 @@ function get_repo_extra_module_packages() {
 
     # Redis2
     if "${NGX_HTTP_REDIS2:-false}"; then
-        echo "Adding ngx-http-redis2 module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-redis2" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-redis2")
         else
             error "${SELECTED_REPO} doesn't have libnginx-mod-http-redis2 module. Skipped..."
@@ -221,20 +222,20 @@ function get_repo_extra_module_packages() {
 
     # Subs filter
     if "${NGX_HTTP_SUBS_FILTER:-false}"; then
-        echo "Adding ngx-http-subs-filter module..."
+        echo "  Adding: libnginx-mod-http-subs-filter" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-subs-filter")
     fi
 
     # Upstream fair
     if "${NGX_HTTP_UPSTREAM_FAIR:-false}"; then
-        echo "Adding ngx-http-upstream-fair module..."
+        echo "  Adding: libnginx-mod-http-upstream-fair" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-upstream-fair")
     fi
 
     # VTS
     if "${NGX_HTTP_VTS:-false}"; then
-        echo "Adding ngx-http-vts module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-vhost-traffic-status" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-vhost-traffic-status")
         else
             error "${SELECTED_REPO} doesn't have libnginx-mod-http-vhost-traffic-status module. Skipped..."
@@ -243,37 +244,40 @@ function get_repo_extra_module_packages() {
 
     # XSLT
     if "${NGX_HTTP_XSLT_FILTER:-false}"; then
-        echo "Adding ngx-http-xslt-filter module..."
+        echo "  Adding: libnginx-mod-http-xslt-filter" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-http-xslt-filter")
     fi
 
     # Mail
     if "${NGX_MAIL:-false}"; then
-        echo "Adding ngx-mail module..."
+        echo "  Adding: libnginx-mod-mail" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-mail")
     fi
 
     # Nchan
     if "${NGX_NCHAN:-false}"; then
-        echo "Adding ngx-nchan module..."
+        echo "  Adding: libnginx-mod-nchan" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-nchan")
     fi
 
     # RTMP
     if "${NGX_RTMP:-false}"; then
-        echo "Adding ngx-rtmp module..."
         if [[ "${SELECTED_REPO}" == "myguard" ]]; then
+            echo "  Adding: libnginx-mod-http-flv-live" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-http-flv-live")
         else
+            echo "  Adding: libnginx-mod-rtmp" >&2
             EXTRA_MODULE_PKGS+=("libnginx-mod-rtmp")
         fi
     fi
 
     # Stream
     if "${NGX_STREAM:-false}"; then
-        echo "Adding ngx-stream module..."
+        echo "  Adding: libnginx-mod-stream" >&2
         EXTRA_MODULE_PKGS+=("libnginx-mod-stream")
     fi
+
+    echo "Total extra modules: ${#EXTRA_MODULE_PKGS[@]}" >&2
 }
 
 ##
@@ -285,12 +289,19 @@ function install_nginx_from_repo() {
     echo "Installing Nginx from ${SELECTED_REPO} repository..."
 
     if [[ -n "${NGINX_PKGS[*]}" ]]; then
+        # Build extra module packages list if enabled
         if "${NGINX_EXTRA_MODULES:-false}"; then
-            echo "Installing Nginx with extra modules..."
+            echo "Checking for extra modules..."
             get_repo_extra_module_packages "${SELECTED_REPO}"
         fi
 
-        # Install Nginx and its modules
-        run apt-get install -q -y "${NGINX_PKGS[@]}" "${EXTRA_MODULE_PKGS[@]}"
+        # Install Nginx packages
+        if [[ ${#EXTRA_MODULE_PKGS[@]} -gt 0 ]]; then
+            echo "Installing Nginx with ${#EXTRA_MODULE_PKGS[@]} extra module packages..."
+            run apt-get install -q -y "${NGINX_PKGS[@]}" "${EXTRA_MODULE_PKGS[@]}"
+        else
+            echo "Installing Nginx (no extra modules)..."
+            run apt-get install -q -y "${NGINX_PKGS[@]}"
+        fi
     fi
 }
